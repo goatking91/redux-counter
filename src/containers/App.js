0939fe88c1031cd35df38520db1ae68865bd7b09
염승민 <tmdals3021@gmail.com>
2019-02-12 01:22:38 +0900

    카운터 생성

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CounterConatiner from './CounterContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <CounterConatiner/>
+      </div>
+    );
+  }
+}
+
+export default App;
